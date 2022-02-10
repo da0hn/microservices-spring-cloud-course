e7f5a9b5b7c4d906b9273b5e2cf2306f3c153869
da0hn <gabrielhondacba@gmail.com>
2022-02-09 20:50:43 -0400

    feat: add inject variable from `application.yml`

---
+++ greeting-service/src/main/java/org/da0hn/greeting/configuration/GreetingConfiguration.java
@@ -0,0 +1,28 @@
+package org.da0hn.greeting.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "greeting-service")
+public class GreetingConfiguration {
+
+  private String greeting;
+  private String defaultValue;
+
+  public String getGreeting() {
+    return greeting;
+  }
+
+  public void setGreeting(String greeting) {
+    this.greeting = greeting;
+  }
+
+  public String getDefaultValue() {
+    return defaultValue;
+  }
+
+  public void setDefaultValue(String defaultValue) {
+    this.defaultValue = defaultValue;
+  }
+}
--- greeting-service/src/main/java/org/da0hn/greeting/controller/GreetingController.java
+++ greeting-service/src/main/java/org/da0hn/greeting/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package org.da0hn.greeting.controller;
 
+import org.da0hn.greeting.configuration.GreetingConfiguration;
 import org.da0hn.greeting.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +14,31 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-  private static final String TEMPLATE = "Hello, %s";
+  private static final String TEMPLATE = "%s, %s";
   private final AtomicLong counter = new AtomicLong();
+
+  private final GreetingConfiguration greetingConfiguration;
+
+  @Autowired
+  public GreetingController(GreetingConfiguration greetingConfiguration) {
+    this.greetingConfiguration = greetingConfiguration;
+  }
 
   @GetMapping
   public Greeting greeting(
-    @RequestParam(value = "name", defaultValue = "World") String name
+    @RequestParam(value = "name", defaultValue = "") String name
   ) {
+
+    if(name.isEmpty()) {
+      name = this.greetingConfiguration.getDefaultValue();
+    }
+
     return new Greeting(
-      String.format(TEMPLATE, name),
+      String.format(
+        TEMPLATE,
+        this.greetingConfiguration.getGreeting(),
+        name
+      ),
       counter.incrementAndGet()
     );
   }
