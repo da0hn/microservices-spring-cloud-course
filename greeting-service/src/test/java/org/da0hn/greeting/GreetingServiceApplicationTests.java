8a625dd1d8817d2a6a195cad371203752808e6da
da0hn <gabrielhondacba@gmail.com>
2022-02-09 20:14:30 -0400

    feat: add spring boot actuator microservice

---
+++ greeting-service/src/test/java/org/da0hn/greeting/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.da0hn.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
