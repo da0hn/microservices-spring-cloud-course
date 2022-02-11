7dadad4780fc1e2f142c91abbaa5acff30b5d87a
da0hn <gabrielhondacba@gmail.com>
2022-02-11 17:44:07 -0400

    feat: add configuration of `flyway` and `mysql` connection

---
+++ cambio-service/src/main/resources/db/migration/V2__Populate_Table_Cambio.sql
@@ -0,0 +1,8 @@
+INSERT INTO `cambio` (`from_currency`, `to_currency`, `conversion_factor`) VALUES
+       ('USD', 'BRL', 5.73),
+       ('USD', 'EUR', 0.84),
+       ('USD', 'GBP', 5.73),
+       ('USD', 'ARS', 5.73),
+       ('USD', 'CLP', 5.73),
+       ('USD', 'COP', 5.73),
+       ('USD', 'MXN', 5.73);
