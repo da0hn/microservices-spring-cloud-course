6697122b63f087ac5585faad2cb9744a09f9d421
da0hn <gabrielhondacba@gmail.com>
2022-02-11 12:21:03 -0400

    feat: add microservice `cambio-service`

---
+++ cambio-service/src/test/java/org/da0hn/cambio/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.da0hn.cambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
