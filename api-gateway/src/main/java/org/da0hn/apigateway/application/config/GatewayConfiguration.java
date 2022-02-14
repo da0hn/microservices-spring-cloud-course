aa15465562542ceb4e091d917bbc64c2191a2f41
da0hn <gabrielhondacba@gmail.com>
2022-02-14 15:17:20 -0400

    feat: add example of route management  on `api-gateway`

---
+++ api-gateway/src/main/java/org/da0hn/apigateway/application/config/GatewayConfiguration.java
@@ -0,0 +1,24 @@
+package org.da0hn.apigateway.application.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+
+  @Bean
+  public RouteLocator gatewayRouter(final RouteLocatorBuilder builder) {
+    return builder.routes()
+      .route(p -> p.path("/get")
+        .filters(f -> f.addRequestHeader("Hello", "World")
+          .addRequestParameter("Hello", "World")
+        )
+        .uri("http://httpbin.org:80"))
+      .build();
+  }
+
+
+}
