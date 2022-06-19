9cf3fd428a41c3943d9f01f806aa61257053fe2c
Gabriel Honda <gabrielhondacba@gmail.com>
2022-06-19 10:12:42 -0400

    feat: add `bulkhead` and `circuit breaker`

--- book-service/src/main/java/org/da0hn/book/application/FooBarController.java
+++ book-service/src/main/java/org/da0hn/book/application/FooBarController.java
@@ -1,6 +1,8 @@
 package org.da0hn.book.application;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +20,27 @@ public class FooBarController {
   private static final Logger LOGGER = LoggerFactory.getLogger(FooBarController.class);
 
   @GetMapping("foo-bar")
+  @Bulkhead(name = "default")
   @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
-  //  @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-  public String getBookById() {
+  @RateLimiter(name = "default")
+  public String fooBar() {
     LOGGER.info("Request to foo-bar is received");
     final var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
     return response.getBody();
+  }
+
+  @GetMapping("foo-baz")
+  @RateLimiter(name = "default")
+  public String fooBaz() {
+    LOGGER.info("Request to foo-baz is received");
+    return "foo-baz";
+  }
+
+  @GetMapping("foo-qux")
+  @Bulkhead(name = "default")
+  public String fooQux() {
+    LOGGER.info("Request to foo-baz is received");
+    return "foo-baz";
   }
 
   public String fallbackMethod(final Exception exception) {
